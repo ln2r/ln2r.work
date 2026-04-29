f930abfb20f908aa850cebaf398038b1c879c22e
ln2r <scorch_62@live.com>
2026-04-29 15:32:25 +0000

    chore: lint cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,63 @@
 <script lang="ts">
-	// import favicon from '$lib/assets/favicon.svg';
-    export const prerender = true
+    // import favicon from '$lib/assets/favicon.svg';
+    export const prerender = true;
 
-    import favicon from '$lib/assets/origami-blank.png';
-    import '@fontsource-variable/jetbrains-mono';
-    import '@fontsource/redacted';
-    import Menu from '$lib/components/menu.svelte';
-    import {random, RandomType} from "$lib/utils/random";
+    import favicon from "$lib/assets/origami-blank.png";
+    import "@fontsource-variable/jetbrains-mono";
+    import "@fontsource/redacted";
+    import Menu from "$lib/components/menu.svelte";
+    import { random, RandomType } from "$lib/utils/random";
 
-	let { children } = $props();
-    let today = $state(new Date);
+    let { children, data } = $props();
+    let today = $state(new Date());
 </script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+<header>
+    <!--    <div class='flavour'>-->
+    <!--        <div class='left'>-->
+    <!--            <p><a href='/' class='secret'>▲</a>  online</p>-->
+    <!--        </div>-->
+    <!--        <div class='right'>-->
+    <!--            <p>WD&nbsp;&nbsp;&nbsp;&nbsp;25/6&nbsp;&nbsp;&nbsp;&nbsp;01</p>-->
+    <!--            <p>PORT-2 - <span class='redacted'>{Math.floor(Math.random() * 1423)}</span></p>-->
+    <!--        </div>-->
+    <!--    </div>-->
+    <div class="block">
+        <div class="info">
+            <div class="cat">
+                <p>
+                    ／l、<br />
+                    （ﾟ､ ｡ ７<br />
+                    　l、~ ヽ<br />
+                    　ししと )ノ<br />
+                </p>
+            </div>
+            <div class="name">
+                <h3>ln2rworks</h3>
+                <p>i do codes yes</p>
+            </div>
+        </div>
+    </div>
+</header>
+<Menu />
+{@render children?.()}
+<footer>
+    <a href="/auth" class="tag"
+        >ln2rworks - {`${random(3, RandomType.LETTERS)}-${today.getUTCFullYear()}${today.getUTCMonth()}${today.getUTCDate()}-${random(12, RandomType.MIXED)}`}</a
+    >
+    <div class="cat">
+        <p>
+            ／l、&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+            （ﾟ､ ｡ ７&nbsp;&nbsp;&nbsp;&nbsp;<br />
+            　l、~ ヽ&nbsp;&nbsp;&nbsp;&nbsp;<br />
+            　ししと )ノ&nbsp;<br />
+        </p>
+    </div>
+</footer>
 
 <style>
     :global(body) {
@@ -21,7 +68,9 @@
             font-size: small;
         }
 
-        h3, p, a {
+        h3,
+        p,
+        a {
             padding: 0;
             margin: 0;
         }
@@ -65,21 +114,23 @@
         margin: 0 4em 4em 0;
         color: darkgrey;
 
-        @media (min-width: 950px){
+        @media (min-width: 950px) {
             z-index: -10;
         }
 
         .cat {
-            @media (min-width: 950px){
+            @media (min-width: 950px) {
                 display: none;
             }
         }
 
         .tag {
+            color: lightslategray;
             font-size: small;
             font-style: normal;
             font-weight: 300;
             line-height: normal;
+            text-decoration: none;
             margin: 0;
 
             @media only screen and (max-width: 600px) {
@@ -88,48 +139,3 @@
         }
     }
 </style>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-<header>
-<!--    <div class='flavour'>-->
-<!--        <div class='left'>-->
-<!--            <p><a href='/' class='secret'>▲</a>  online</p>-->
-<!--        </div>-->
-<!--        <div class='right'>-->
-<!--            <p>WD&nbsp;&nbsp;&nbsp;&nbsp;25/6&nbsp;&nbsp;&nbsp;&nbsp;01</p>-->
-<!--            <p>PORT-2 - <span class='redacted'>{Math.floor(Math.random() * 1423)}</span></p>-->
-<!--        </div>-->
-<!--    </div>-->
-    <div class='block'>
-        <div class='info'>
-            <div class='cat'>
-                <p>
-                    　／l、<br />
-                    （ﾟ､ ｡ ７<br />
-                    　l、~ ヽ<br />
-                    　ししと )ノ<br />
-                </p>
-            </div>
-            <div class='name'>
-                <h3>ln2rworks</h3>
-                <p>i do codes yes</p>
-            </div>
-        </div>
-    </div>
-</header>
-<Menu />
-{@render children?.()}
-<footer>
-    <p class='tag'>ln2rworks - {`${random(3, RandomType.LETTERS)}-${today.getUTCFullYear()}${today.getUTCMonth()}${today.getUTCDate()}-${random(12, RandomType.MIXED)}`}</p>
-    <div class='cat'>
-        <p>
-            　／l、&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
-            （ﾟ､ ｡ ７&nbsp;&nbsp;&nbsp;&nbsp;<br />
-            　l、~ ヽ&nbsp;&nbsp;&nbsp;&nbsp;<br />
-            　ししと )ノ&nbsp;<br />
-        </p>
-    </div>
-</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import SvelteMarkdown from "@humanspeak/svelte-markdown";
-    import {formatDate} from "$lib/utils/format-date.ts";
+    import { formatDate } from "$lib/utils/format-date.ts";
 
     export let data;
 </script>
@@ -8,6 +8,83 @@
 <svelte:head>
     <title>ln2rworks</title>
 </svelte:head>
+
+<div class="content">
+    <p>
+        heavily and mainly doing backend nodejs stuff, from professional use
+        (work) to silly random app i make to make things easier. outside codes i
+        love to play games, watch movies/anime, and read books.
+    </p>
+
+    <div class="project-list">
+        <h2>stuff i did</h2>
+        {#await data.projects}
+            <p>getting stuff i did...</p>
+        {:then data}
+            {#if data.length > 0 && data[0].length > 0}
+                {#each data as group}
+                    <div class="project-row">
+                        {#each group as project}
+                            <div class="project-item">
+                                <h3>
+                                    > <a href={project.url}>{project.title}</a>
+                                </h3>
+                                <p>
+                                    {project.description ??
+                                        "no description added"}
+                                </p>
+                                <br />
+                                <small>{project.stacks}</small>
+                            </div>
+                        {/each}
+                    </div>
+                {/each}
+            {:else}
+                <p>it seems i have not did anything yet.</p>
+            {/if}
+        {:catch error}
+            <p>the list seems broken... will get back to you soon.</p>
+        {/await}
+    </div>
+
+    <div class="writing-list">
+        <h2>writings</h2>
+        {#await data.writings}
+            <p>getting writings...</p>
+        {:then data}
+            {#if data.length > 0}
+                {#each data as writing}
+                    <div class="writing-item">
+                        <h3>
+                            / <a href="/blog/{writing.slug}">{writing.title}</a>
+                        </h3>
+                        <small>Posted: {formatDate(writing.created)}</small>
+                        <p>
+                            <SvelteMarkdown
+                                source={writing.body.slice(0, 50) + "..."}
+                            />
+                        </p>
+                    </div>
+                {/each}
+            {:else}
+                <p>it seems i have not written anything yet.</p>
+            {/if}
+        {:catch error}
+            <p>whoops</p>
+        {/await}
+    </div>
+
+    <div class="contact-list">
+        <h2>contacts</h2>
+        <div class="contact-item">
+            <p>
+                <a href="https://discord.com/users/132712988875423745"
+                    >discord</a
+                >
+            </p>
+        </div>
+    </div>
+</div>
 
 <style>
     .content {
@@ -20,11 +97,14 @@
         }
     }
 
-    .project-list, .writing-list, .contact-list {
+    .project-list,
+    .writing-list,
+    .contact-list {
         margin-top: 3em;
     }
 
-    .project-item, .contact-item {
+    .project-item,
+    .contact-item {
         h3 {
             margin: 0;
         }
@@ -54,7 +134,8 @@
             margin-bottom: 0;
         }
 
-        p, small {
+        p,
+        small {
             padding-left: 1.3em;
         }
 
@@ -73,61 +154,3 @@
         }
     }
 </style>
-
-<div class="content">
-    <p>heavily and mainly doing backend nodejs stuff, from professional use (work) to silly random app i make to make things easier. outside codes i love to play games, watch movies/anime, and read books.</p>
-
-    <div class="project-list">
-        <h2>stuff i did</h2>
-        {#await data.projects}
-            <p>getting stuff i did...</p>
-            {:then data}
-                {#if data.length > 0}
-                    {#each data as group}
-                        <div class="project-row">
-                            {#each group as project}
-                                <div class="project-item">
-                                    <h3>> <a href={project.html_url}>{project.name}</a> </h3>
-                                    <p>{project.description ?? "no description added"}</p>
-                                    <br />
-                                    <small>{[project.language, ...project.topics].join (", ")}</small>
-                                </div>
-                            {/each}
-                        </div>
-                    {/each}
-                {:else}
-                    <p>it seems i have not did anything yet.</p>
-                {/if}
-            {:catch error}
-            <p>the list seems broken... will get back to you soon.</p>
-        {/await}
-    </div>
-
-    <div class="writing-list">
-        <h2>writings</h2>
-        {#await data.writings}
-            <p>getting writings...</p>
-            {:then data}
-                {#if data.length > 0}
-                    {#each data as writing}
-                        <div class="writing-item">
-                            <h3>/ <a href="/blog/{writing.slug}">{writing.title}</a></h3>
-                            <small>Posted: {formatDate(writing.created)}</small>
-                            <p><SvelteMarkdown source={writing.body.slice(0, 50) + "..."} /></p>
-                        </div>
-                    {/each}
-                {:else}
-                    <p>it seems i have not written anything yet.</p>
-                {/if}
-            {:catch error}
-            <p>whoops</p>
-        {/await}
-    </div>
-
-    <div class="contact-list">
-        <h2>contacts</h2>
-        <div class="contact-item">
-            <p><a href="https://discord.com/users/132712988875423745">discord</a></p>
-        </div>
-    </div>
-</div>
